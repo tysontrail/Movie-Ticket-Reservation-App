2ddfa7c118d2db9e7419ace8f872692f09c3314b
aamanuel1 <aaron.manuel@ucalgary.ca>
2022-11-29 22:09:50 -0700

    First try at Theatre controller, service and repository, has add functionality but there's an issue with the theatre resultset (something to do with tickets).

---
+++ assignment6/src/main/java/course/ensf607/assignment6/controller/TheatreController.java
@@ -0,0 +1,57 @@
+package course.ensf607.assignment6.controller;
+
+import course.ensf607.assignment6.entity.Movie;
+import course.ensf607.assignment6.entity.Theatre;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin({"*"})
+public class TheatreController {
+
+    @Autowired
+    private TheatreService theatreService;
+
+    @Autowired
+    public TheatreController(TheatreService theatreService){
+        this.theatreService = theatreService;
+    }
+
+    //View all theatres and search theatres can't be viewed due to some issues with
+    //the result set eg tickets and seats haven't been created yet.
+    //TODO Fix.
+    @GetMapping({"api/v1/viewalltheatres"})
+    public Iterable<Theatre> viewAllTheatres(){
+        return this.theatreService.getAllTheatres();
+    }
+
+    @GetMapping({"/api/v1/searchtheatres"})
+    public Optional<Theatre> searchTheatres(@RequestParam String name){
+        return this.theatreService.searchTheatreByName(name);
+    }
+
+    //Not yet implemented.
+    @PostMapping({"/api/v1/selecttheatre"})
+    public void selectTheatre(@RequestBody Theatre theTheatre){
+        this.theatreService.selectTheatre(theTheatre);
+    }
+
+    @PostMapping({"/api/v1/addtheatre"})
+    public void addTheatre(@RequestBody Theatre theatre){
+        this.theatreService.addTheatre(theatre.getName(), theatre.getAddress(), theatre.getSeatCols(),
+                theatre.getSeatRows());
+    }
+
+    @PostMapping({"/api/v1/deletetheatre"})
+    public void deleteTheatre(@RequestParam long id){
+        this.theatreService.deleteTheatre(id);
+    }
+
+    @PostMapping({"/api/v1/addmovietotheatre"})
+    public void addTheatreToMovie(Movie movie){
+
+    }
+
+}
---
+++ assignment6/src/main/java/course/ensf607/assignment6/controller/TheatreService.java
@@ -0,0 +1,56 @@
+package course.ensf607.assignment6.controller;
+
+import course.ensf607.assignment6.entity.Theatre;
+import course.ensf607.assignment6.repository.TheatreRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TheatreService {
+
+    private final TheatreRepository theatreRepository;
+
+    public TheatreService(TheatreRepository theatreRepository){
+        this.theatreRepository = theatreRepository;
+    }
+
+    public Optional<Theatre> searchTheatreByName(String name){
+        return this.searchTheatreByName(name);
+    }
+
+    public Optional<Theatre> searchTheatreByID(long id){
+        return this.theatreRepository.findTheatreById(id);
+    }
+
+    public void selectTheatre(Theatre selectedTheatre){
+        //TODO: include logic for storing the theatre in the ticket.
+        //Needs to get stored in ticket.
+    }
+
+    public void addTheatre(Theatre theatre){
+        Optional<Theatre> checkTheatre = searchTheatreByID(theatre.getId());
+        if(checkTheatre.isPresent()){
+            throw new IllegalStateException("Theatre ID already present in database.");
+        }
+    }
+
+    public void addTheatre(String name, String address, int seatCols, int seatRows){
+        Theatre newTheatre = new Theatre(name, address, seatCols, seatRows);
+        Optional<Theatre> matchingTheatreName = theatreRepository.findByName(name);
+        if(matchingTheatreName.isPresent()) {
+            throw new IllegalStateException("Theatre name already in database.");
+        }
+        this.theatreRepository.save(newTheatre);
+    }
+
+    public void deleteTheatre(long id){
+        this.theatreRepository.deleteById(id);
+    }
+
+    public Iterable<Theatre> getAllTheatres(){
+        return this.theatreRepository.findAll();
+    }
+
+
+}
