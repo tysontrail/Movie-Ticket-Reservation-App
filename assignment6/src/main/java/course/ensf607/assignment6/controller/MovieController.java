df1c373c413202f8399059d6fbdc0a2ddc55b78b
aamanuel1 <aaron.manuel@ucalgary.ca>
2022-11-29 19:48:14 -0700

    Implemented search and add capability for Movie related classes, still need to implement select movie

---
+++ assignment6/src/main/java/course/ensf607/assignment6/controller/MovieController.java
@@ -0,0 +1,58 @@
+package course.ensf607.assignment6.controller;
+
+import course.ensf607.assignment6.entity.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin({"*"})
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService){
+        this.movieService = movieService;
+    }
+
+//    @GetMapping({"/api/v1/searchmovie"})
+//    public Optional<Movie> searchAnnouncedMovie(@RequestParam String name){
+//        return this.movieService.searchAnnouncedMovie(name);
+//    }
+
+    @GetMapping({"/api/v1/searchallmovies"})
+    public Optional<Movie> searchAllMovies(@RequestParam String name){
+        return this.movieService.searchAllMovies(name);
+    }
+
+    @PostMapping({"/api/v1/selectmovie"})
+    public void selectMovie(@RequestBody Movie theMovie){
+        this.movieService.selectMovie(theMovie);
+    }
+
+    @PostMapping({"/api/v1/addmovie"})
+    public void addMovie(@RequestBody Movie movie){
+        this.movieService.addMovie(movie.getName(), movie.getDescription(), movie.getPrivateStart(),
+                movie.getPublicStart());
+    }
+
+    @PostMapping({"/api/v1/deletemovie"})
+    public void deleteMovie(long id){
+        this.movieService.deleteMovie(id);
+    }
+
+    @GetMapping({"api/v1/viewannouncedmovies"})
+    public Iterable<Movie> viewAnnouncedMovies(){
+        return this.movieService.getAnnouncedMovies();
+    }
+
+    @GetMapping({"api/v1/viewallmovies"})
+    public Iterable<Movie> viewAllMovies(){
+        return this.movieService.getAllMovies();
+    }
+}
+
+
---
+++ assignment6/src/main/java/course/ensf607/assignment6/repository/MovieRepository.java
@@ -0,0 +1,22 @@
+package course.ensf607.assignment6.repository;
+
+import course.ensf607.assignment6.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    Optional<Movie> findMovieById(Long id);
+
+    //For search all movies
+    Optional<Movie> findMovieByName(String name);
+
+    //For search announced movies only for reg users. Uses jpa custom query.
+    @Query(value="SELECT * FROM movie m WHERE m.public_start <= CURDATE();", nativeQuery = true)
+    List<Movie> findAllGreaterThanPublicStart();
+
+}
