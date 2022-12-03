4b1ba5a797bf67f10c90ec3281e8322228cf698f
aamanuel1 <aaron.manuel@ucalgary.ca>
2022-12-03 10:39:22 -0700

    Service class changes to allow for select and store in ticket methods

--- assignment6/src/main/java/course/ensf607/assignment6/service/ShowtimeService.java
+++ assignment6/src/main/java/course/ensf607/assignment6/service/ShowtimeService.java
@@ -3,7 +3,9 @@ package course.ensf607.assignment6.service;
 import course.ensf607.assignment6.entity.Movie;
 import course.ensf607.assignment6.entity.Showtime;
 import course.ensf607.assignment6.entity.Theatre;
+import course.ensf607.assignment6.entity.Ticket;
 import course.ensf607.assignment6.repository.ShowtimeRepository;
+import course.ensf607.assignment6.repository.TicketRepository;
 import course.ensf607.assignment6.service.MovieService;
 import course.ensf607.assignment6.service.TheatreService;
 import java.time.LocalDateTime;
@@ -16,17 +18,46 @@ public class ShowtimeService {
   private final TheatreService theatreService;
   private final MovieService movieService;
   private final ShowtimeRepository showtimeRepository;
+  //TODO: May need to delete the ticket repositories after testing is complete and everything is working.
+  private final TicketRepository ticketRepository;
 
   public ShowtimeService(
       TheatreService theatreService,
       ShowtimeRepository showtimeRepository,
-      MovieService movieService) {
+      MovieService movieService,
+      TicketRepository ticketRepository) {
     this.theatreService = theatreService;
     this.showtimeRepository = showtimeRepository;
     this.movieService = movieService;
+    this.ticketRepository = ticketRepository;
   }
 
-  public void selectShowtime() {}
+  //Showtime object version
+  public void selectShowtime(Showtime showtime) {
+    Ticket theTicket = Ticket.getInstance();
+    //Probably have to find showtime by id because of the relationships.
+    Optional<Showtime> theShowtime = showtimeRepository.findById(showtime.getId());
+    if(theShowtime.isPresent()){
+      theTicket.setShowtime(showtime.getStartTime());
+      this.ticketRepository.save(theTicket);
+    }
+    else{
+      throw new IllegalStateException("Could not find showtime.");
+    }
+  }
+
+  //Showtime Id version
+  public void selectShowtime(long showtimeId) {
+    Ticket theTicket = Ticket.getInstance();
+    //Probably have to find showtime by id because of the relationships.
+    Optional<Showtime> theShowtime = showtimeRepository.findById(showtimeId);
+    if (theShowtime.isPresent()) {
+      theTicket.setShowtime(theShowtime.get().getStartTime());
+      this.ticketRepository.save(theTicket);
+    } else {
+      throw new IllegalStateException("Could not find showtime.");
+    }
+  }
 
   //    public Iterable<Showtime> getAllTheatreShowtimes(String theatreName){
   //
@@ -48,7 +79,6 @@ public class ShowtimeService {
       throw new IllegalStateException("Could not find theatre.");
     } else {
       Showtime newShowtime = new Showtime(startTime, addTheatre.get());
-      ;
       newShowtime.setTheatre(addTheatre.get());
       this.showtimeRepository.save(newShowtime);
       return Optional.of(newShowtime);
